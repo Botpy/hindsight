d07c92576706a6aeb113073bbd4eeb6b568c82a9
wanghui <wh_linux@126.com>
2017-12-26 16:42:07 +0800

    Add buildbot9 support

--- hindsight/deployment.py
+++ hindsight/deployment.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """Deployment webhook."""
-from __future__ import print_function, division, unicode_literals
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 
+import base64
 import json
 
 import enum
 
-from tornado import web
-from tornado import gen
-from tornado.log import gen_log
-
-from asyncat.repository import Repository
 from asyncat.client import GithubError
+from asyncat.repository import Repository
+from tornado import gen
+from tornado import web
+from tornado.log import gen_log
 
 from .finder import NoSuchPullRequest
 
 
 class BuildStatus(enum.Enum):
+    """Build Status."""
     unknow = "unknow"
     pending = "pending"
     success = "success"
@@ -33,6 +37,9 @@ class BaseCIBuild(object):
         """
         self.payload = payload
         self.prepare()
+
+    def prepare(self):
+        pass
 
     def get_name(self):
         """Returns the name of current builder.  Returns ``None`` if current
@@ -80,11 +87,28 @@ class BaseCIWebhook(object):
 class BuildbotBuild(BaseCIBuild):
     """Represents a build of buildbot."""
     def prepare(self):
-        self.event = self.payload["event"]
-        self.info = self.payload["payload"]["build"]
-        self.properties = dict(x[:2] for x in self.info['properties'])
-        self.sha = (self.properties["revision"] or
-                    self.properties.get("got_revision"))
+        self.is_nine = self.payload.get("is_nine", False)
+        if self.is_nine:
+            if self.payload["complete"]:
+                self.event = "buildFinished"
+            else:
+                self.event = "buildStarted"
+
+            self.info = {
+                "results": self.payload["results"],
+                "text": self.payload["state_string"],
+            }
+            self.properties = {
+                k: v[0] for k, v in self.payload["properties"].items()
+            }
+        else:
+            self.event = self.payload["event"]
+            self.info = self.payload["payload"]["build"]
+            self.properties = dict(x[:2] for x in self.info['properties'])
+        self.sha = (
+            self.properties["revision"] or
+            self.properties.get("got_revision")
+        )
 
     def get_name(self):
         return self.properties["buildername"]
@@ -108,7 +132,33 @@ class BuildbotBuild(BaseCIBuild):
 
 
 class BuildbotWebhook(BaseCIWebhook):
+    def _decode_user_and_secret(self):
+        """Decode user and secret from Authorization header."""
+        authorization = self.handler.request.headers["Authorization"]
+        if not authorization.startswith("Basic"):
+            raise ValueError("Not Basic authorization")
+        return base64.b64decode(
+            authorization[6:].encode('utf8')
+        ).decode("utf8").split(":")
+
+    @property
+    def is_nine(self):
+        """Returns True if is buildbot 9."""
+        # If contains Authorization header, that is buildbot 9
+        if "Authorization" in self.handler.request.headers:
+            try:
+                user, _ = self._decode_user_and_secret()
+            except ValueError:
+                pass
+            else:
+                return user == "buildbot"
+
+        return False
+
     def get_secret(self):
+        if self.is_nine:
+            return self._decode_user_and_secret()[1]
+
         return self.handler.get_argument("secret")
 
     def iter_builds(self):
@@ -116,13 +166,23 @@ class BuildbotWebhook(BaseCIWebhook):
 
         :rtype: :class:`BuildbotBuild`
         """
+        if self.is_nine:
+            payload = json.loads(self.handler.request.body)
+            payload["is_nine"] = self.is_nine
+            build = BuildbotBuild(payload)
+            if build.is_valid():
+                yield build
+            return
+
         packets = json.loads(self.handler.get_argument("packets"))
         for payload in packets:
             if payload["event"] not in ["buildStarted", "buildFinished"]:
                 continue
 
+            payload["is_nine"] = self.is_nine
+
             build = BuildbotBuild(payload)
-            if not build.get_sha():
+            if not build.is_valid():
                 continue
             yield build
 
@@ -165,37 +225,47 @@ class DeploymentHandler(web.RequestHandler):
         """
 
         secret = hook.get_secret()
-
+        gen_log.info("Got secret from hook %s", secret)
         try:
-            config = self.application.find_repo_config(secret,
-                                                       build.get_name())
+            config = self.application.find_repo_config(
+                secret,
+                build.get_name(),
+            )
         except KeyError:
             gen_log.warn("Could not find config with secret: %s.", secret)
             self.write("Secret mismatch.")
             raise web.HTTPError(403)
 
-        return Repository(self.application.github_client, config["owner"],
-                          config["name"])
+        return Repository(
+            self.application.github_client, config["owner"],
+            config["name"],
+        )
 
     @gen.coroutine
     def _on_build(self, hook, build):
         repo = self._get_repo(hook, build)
 
-        gen_log.info("Try find pull requset via %s in %s/%s", build.get_sha(),
-                     repo.owner, repo.label)
+        gen_log.info(
+            "Try find pull requset via %s in %s/%s", build.get_sha(),
+            repo.owner, repo.label,
+        )
 
         try:
             pull = yield self.application.find_pull(repo, build.get_sha())
         except (GithubError, NoSuchPullRequest) as e:
-            gen_log.error("Could not find any pull request via %s in %s/%s",
-                          build.get_sha(), repo.owner, repo.label,
-                          exc_info=True)
+            gen_log.error(
+                "Could not find any pull request via %s in %s/%s",
+                build.get_sha(), repo.owner, repo.label,
+                exc_info=True,
+            )
             if isinstance(e, GithubError):
                 raise web.HTTPError(404)
             return
 
-        gen_log.info("Found pull request #%s via %s in %s/%s", pull.num,
-                     build.get_sha(), repo.owner, repo.label)
+        gen_log.info(
+            "Found pull request #%s via %s in %s/%s", pull.num,
+            build.get_sha(), repo.owner, repo.label,
+        )
 
         comment = "Deployment status {}".format(build.get_status())
         yield pull.create_comment(comment)
--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """Tests of deployment."""
-from __future__ import print_function, division, unicode_literals
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import base64
 
 import mock
 
@@ -13,11 +18,14 @@ from . import HindsightTestCase
 
 
 class BuildbotTestCase(HindsightTestCase):
+    """Tests for buildbot."""
     def _get_packets(self):
+        """Returns test packets."""
         with open(self.get_file_path("_buildbot-packets.json")) as f:
             return f.read()
 
     def test_secret_mismatch(self):
+        """Secret mismatch should returns 403."""
         resp = self.fetch("/deployment", body=self.make_body({
             "secret": "secret",
             "packets": self._get_packets()
@@ -25,6 +33,7 @@ class BuildbotTestCase(HindsightTestCase):
         self.assertEqual(resp.code, 403)
 
     def _push(self):
+        """Push event."""
         return self.fetch("/deployment", body=self.make_body({
             "secret": "mock-secret",
             "packets": self._get_packets()
@@ -32,6 +41,7 @@ class BuildbotTestCase(HindsightTestCase):
 
     @mock.patch("hindsight.app.Application.find_pull", autospec=True)
     def test_pull_not_found(self, mock_find_pull):
+        """Pull request not found."""
         mock_find_pull.return_value = self.make_future(NoSuchPullRequest())
 
         resp = self._push()
@@ -44,6 +54,7 @@ class BuildbotTestCase(HindsightTestCase):
 
     @mock.patch("hindsight.finder.PullRequestFinder.find", autospec=True)
     def test_find_pull_via_sha(self, mock_find):
+        """Find pull request via sha in event."""
         mock_pull_cls = mock.create_autospec("asyncat.repository.PullRequest")
         mock_pull = mock_pull_cls.return_value
         mock_pull.create_comment.return_value = self.make_future(None)
@@ -51,5 +62,70 @@ class BuildbotTestCase(HindsightTestCase):
         mock_find.return_value = self.make_future(mock_pull)
 
         self._push()
+
+        self.assertTrue(mock_pull.create_comment.called)
+
+
+class Buildbot9TestCase(HindsightTestCase):
+    """Buildbot 9 test case."""
+    def _get_payload(self, type_="done"):
+        """Returns test payload."""
+        with open(self.get_file_path("_buildbot9-{}.json".format(type_))) as f:
+            return f.read()
+
+    def test_secret_mismatch(self):
+        """Secret mismatch should returns 403."""
+        resp = self.fetch(
+            "/deployment",
+            body=self._get_payload(),
+            headers={
+                "Authorization": "Basic {}".format(
+                    base64.b64encode(
+                        "buildbot:{}".format("secret").encode("utf8")
+                    ).decode("utf8"),
+                )
+            },
+            method="POST",
+        )
+        self.assertEqual(resp.code, 403)
+
+    def _push(self, type_):
+        """Push event."""
+        return self.fetch(
+            "/deployment",
+            body=self._get_payload(type_),
+            headers={
+                "Authorization": "Basic {}".format(
+                    base64.b64encode(
+                        "buildbot:{}".format("mock-secret").encode("utf8"),
+                    ).decode("utf8"),
+                )
+            },
+            method="POST",
+        )
+
+    @mock.patch("hindsight.app.Application.find_pull", autospec=True)
+    def test_pull_not_found(self, mock_find_pull):
+        """Could not found pull request."""
+        mock_find_pull.return_value = self.make_future(NoSuchPullRequest())
+
+        resp = self._push("done")
+        self.assertEqual(resp.code, 200)
+        self.assertEqual(resp.body, b'OK')
+
+        mock_find_pull.return_value = self.make_future(GithubError())
+        resp = self._push("done")
+        self.assertEqual(resp.code, 404)
+
+    @mock.patch("hindsight.finder.PullRequestFinder.find", autospec=True)
+    def test_find_pull_via_sha(self, mock_find):
+        """Find pull request via sha in event."""
+        mock_pull_cls = mock.create_autospec("asyncat.repository.PullRequest")
+        mock_pull = mock_pull_cls.return_value
+        mock_pull.create_comment.return_value = self.make_future(None)
+
+        mock_find.return_value = self.make_future(mock_pull)
+
+        self._push("done")
 
         self.assertTrue(mock_pull.create_comment.called)
